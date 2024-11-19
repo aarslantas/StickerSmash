3c8f177ef08e2e9414271fd793d84fa0b8d5f569
aarslantas <a_arslantas69@hotmail.com>
2024-11-19 21:46:53 +0300

    create imageViewer

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,31 @@
-import { Image, StyleSheet, Platform, View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import ImageViewer from "@/components/ImageViewer";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
-export default function HomeScreen() {
+export default function Index() {
   return (
-    <View>
-      <Text>bayburt</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
